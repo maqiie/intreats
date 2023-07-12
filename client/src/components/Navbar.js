914425d197496596b0af902c86fe670568363632
maqiie <candiemp2580@gmail.com>
2023-07-12 18:35:42 +0300

    working on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,31 @@
-import React from "react";
 
+import React from "react";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
-    return(
-        <nav className="navbar navbar-expand-lg bg-dark">
-
-        </nav>
-    )
+  return (
+    <nav className="bg-white text-gray fixed top-0 left-0 w-full z-10 mx-auto px-6 py-4 flex justify-between items-center mt-10">
+      <a className="navbar-brand h-13 w-14 mr-2 flex items-center" href="/">
+      {/* <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+      </div> */}
+        <img src={logo} alt="Logo" className="h-full" />
+      </a>
+      {/* Add other navigation components or links */}
+      <a className="" href="home">
+        Home
+      </a>
+      <a className="" href="menu">
+        Menu
+      </a>
+      <a className="" href="about-us">
+        About Us
+      </a>
+      <a className="" href="contact-us">
+        Contact Us
+      </a>
+      
+    </nav>
+  );
 };
 
 export default Navbar;
