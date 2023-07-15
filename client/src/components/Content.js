91da5f8aba3392502de2e445b78cf244b28686c7
maqiie <candiemp2580@gmail.com>
2023-07-15 03:06:25 +0300

    fiished the content

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,45 +1,31 @@
-// import React from "react";
-
-// const Content = () => {
-//   return (
-//     <div className="text-center mt-10">
-//       <h1 className="text-3xl font-bold text-blue-500 ">
-//         WHENEVER WE BAKE, BAKE WITH OUR HEART
-//       </h1>
-
-//       <div className="my-8">
-//         <p className="text-white dark:text-gray-400">
-//           WHENEVER WE BAKE, BAKE WITH OUR HEART
-//         </p>
-//         <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
-//         <p className="text-gray-500 dark:text-gray-400 font-bold text-2xl">
-//           "Intreats: Delighting Taste Buds, One Sweet Surprise at a Time!"
-//         </p>
-
-//         <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Content;
 import React from "react";
+import { Styles } from "./Content.css";
 
 const Content = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-     
-
-      <div className="my-8 ">
-        <p className="text-white dark:text-gray-400">
+      <div className=" Content  ">
+        <p className=" text text-white dark:text-gray-400">
           WHENEVER WE BAKE, BAKE WITH OUR HEART
         </p>
         <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
-        <p className="text-gray-500 dark:text-white font-bold text-4xl text-center">
-          "Intreats: Delighting Taste Buds, One Sweet Surprise at a Time!"
+        <p className=" theme text-gray-500 dark:text-white font-bold text-4xl text-center">
+          "InJoy: Delighting Taste Buds, One Sweet Surprise at a Time!"
         </p>
 
         <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
+        <p className=" text text-white dark:text-gray-400">
+          "Welcome to Injoy Treats, where our baked goods are made with passion,
+          precision, and a sprinkle of happiness."
+        </p>
+        <br/>
+        <br>
+        </br>
+        <button class="button">
+          <div class="btn-circle"></div>
+          <div class="btn-circle"></div>
+          <p class="btn-text">CHECK OUR MENU</p>
+        </button>
       </div>
     </div>
   );
--- client/src/components/socials.js
+++ client/src/components/socials.js
@@ -1,39 +1,4 @@
-// import React from "react";
-// import { FaTwitter, FaInstagram } from "react-icons/fa";
-// import { SiTiktok } from "react-icons/si";
-// import { PiWhatsappLogoDuotone } from "react-icons/pi";
-// import { Styles } from "./socials.css";
 
-// const Socials = () => {
-//   return (
-//     <nav className="socials flex justify-between  items-center z-10 mx-10">
-//       <div className="flex gap-4 ">
-//         <a href="https://twitter.com">
-//           <FaTwitter size={12} color="#1DA1F2" />
-//         </a>
-//         <a href="https://instagram.com">
-//           <FaInstagram size={12} color="#C13584" />
-//         </a>
-//         <a href="https://tiktok.com">
-//           <SiTiktok size={12} color="white" />
-//         </a>
-//         <a href="https://whatsapp.com">
-//           <PiWhatsappLogoDuotone size={12} color="white" />
-//         </a>
-//         <a className="ml-4" >
-//           +254712355467
-//         </a>
-//         <a className="ml-4" >
-//           intreats@gmail.com
-//         </a>
-//       </div>
-
-     
-//     </nav>
-//   );
-// };
-
-// export default Socials;
 import React from "react";
 import { FaTwitter, FaInstagram } from "react-icons/fa";
 import { SiTiktok } from "react-icons/si";
